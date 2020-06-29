7b0f8742d299e7328327fc2fa97bf89dbf0b76a6
Mykola Gnatiuk <gmstudio1916@gmail.com>
2020-06-29 22:57:14 +0200

    [feat] Fix text issue.

--- js/script.js
+++ js/script.js
@@ -25,11 +25,11 @@ function startVideo(){
 }
  
 Promise.all([
-    faceapi.nets.tinyFaceDetector.loadFromUri("https://github.com/lodomur/AI-Emotions-Detector/blob/master/models/"),
-    faceapi.nets.faceLandmark68Net.loadFromUri("https://github.com/lodomur/AI-Emotions-Detector/blob/master/models/"),
-    faceapi.nets.faceRecognitionNet.loadFromUri("https://github.com/lodomur/AI-Emotions-Detector/blob/master/models/"),
-    faceapi.nets.faceExpressionNet.loadFromUri("https://github.com/lodomur/AI-Emotions-Detector/blob/master/models/"),
-    faceapi.nets.ageGenderNet.loadFromUri("https://github.com/lodomur/AI-Emotions-Detector/blob/master/models/")
+    faceapi.nets.tinyFaceDetector.loadFromUri("~/models/"),
+    faceapi.nets.faceLandmark68Net.loadFromUri("~/models/"),
+    faceapi.nets.faceRecognitionNet.loadFromUri("~/models/"),
+    faceapi.nets.faceExpressionNet.loadFromUri("~/models/"),
+    faceapi.nets.ageGenderNet.loadFromUri("~/models/")
 ]).then(startVideo);
 
 function showExpression({ expressions }) {
